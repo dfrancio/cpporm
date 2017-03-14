0061d15f5d834f695042393dc62c233f5fe15d36
Diego Sogari <diego.sogari@gmail.com>
2017-03-13 23:44:18 -0300

    Update googletest setup script

--- cmake/Download/googletest/postbuild.cmake
+++ cmake/Download/googletest/postbuild.cmake
@@ -4,12 +4,27 @@ endif()
 
 FILE(READ ${googletest_DIR}/src/googletest/CMakeLists.txt CONTENT)
 if(NOT CONTENT MATCHES "cmake_policy")
-    STRING(REGEX REPLACE "project\\(" "cmake_policy(SET CMP0048 NEW)\nproject(" CONTENT ${CONTENT})
+    STRING(REGEX REPLACE "(cmake_minimum_required[^\n]*)" "\\1\ncmake_policy(SET CMP0048 NEW)" CONTENT ${CONTENT})
     FILE(WRITE ${googletest_DIR}/src/googletest/CMakeLists.txt ${CONTENT})
 endif()
 
+if(APPLE)
+    FILE(READ ${googletest_DIR}/src/googletest/googletest/CMakeLists.txt CONTENT)
+    if(NOT CONTENT MATCHES "cmake_policy")
+        STRING(REGEX REPLACE "(cmake_minimum_required[^\n]*)" "\\1\ncmake_policy(SET CMP0042 NEW)" CONTENT ${CONTENT})
+        FILE(WRITE ${googletest_DIR}/src/googletest/googletest/CMakeLists.txt ${CONTENT})
+    endif()
+
+    FILE(READ ${googletest_DIR}/src/googletest/googlemock/CMakeLists.txt CONTENT)
+    if(NOT CONTENT MATCHES "cmake_policy")
+        STRING(REGEX REPLACE "(cmake_minimum_required[^\n]*)" "\\1\ncmake_policy(SET CMP0042 NEW)" CONTENT ${CONTENT})
+        FILE(WRITE ${googletest_DIR}/src/googletest/googlemock/CMakeLists.txt ${CONTENT})
+    endif()
+endif()
+
 add_subdirectory(${googletest_DIR}/src/googletest
-                 ${googletest_DIR}/src/googletest-build)
+                 ${googletest_DIR}/src/googletest-build
+                 EXCLUDE_FROM_ALL)
 
 set(googletest_INCLUDE_DIR
     "${gmock_SOURCE_DIR}/include"
